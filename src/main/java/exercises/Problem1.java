bd444372146ad9ca622fa2e81122100be8043e8c
PpMoy <josemariamoyano.ar@gmail.com>
2024-07-16 21:32:40 +0200

    Project-euler exercise 1

--- src/main/java/exercises/Problem1.java
+++ src/main/java/exercises/Problem1.java
@@ -6,6 +6,21 @@ public class Problem1 {
     Find the sum of all the multiples of $3$ or $5$ below $1000$.
      */
     public static void main(String[] args) {
+        int sumatoria = 0;
+        for (int i = 0; i < 1000; i++) {
+            if (is3Multiply(i) || is5Multiply(i)) {
+                sumatoria = sumatoria + i;
+            }
+        }
+        System.out.println("El resultado es " + sumatoria);
+    }
 
+    public static boolean is3Multiply(int num) {
+        return (num % 3) == 0;
+    }
+
+    public static boolean is5Multiply(int num) {
+        return (num % 5) == 0;
     }
 }
+
